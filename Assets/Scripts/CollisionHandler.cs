1c4f4c320e1f5543c21d68b617a02c7d550b0e30
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-05 07:30:37 +0100

    Level Management
    Added scene loading and disabled controls upon crash.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    Movement movementScript;
+
+    private void Awake()
+    {
+        movementScript = GetComponent<Movement>();
+    }
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -12,11 +19,36 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This is s a friendly object");
                 break;
             case "Finished":
-                Debug.Log("This is the landing pad object");
+                LoadNextLevel();
                 break;
             default:
-                Debug.Log("You blew up");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void StartCrashSequence()
+    {
+        movementScript.enabled = false;
+        Invoke("ReloadLevel", 1.5f);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
